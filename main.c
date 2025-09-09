7bbbdce8b8644b6c2ad6586658c2cb15ee54291f
Mantas Motuzas <mantas.motuzas41@gmail.com>
2025-09-09 22:30:27 +0300

    read from socket

--- main.c
+++ main.c
@@ -1,30 +1,84 @@
 #include <stdio.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int main() {
-    FILE *fileptr = fopen("./messages.txt", "r");
+#define MAX_MESSAGE_SIZE 512
+#define MAX_QUEUED_REQUESTS 5
 
-    char byte[9];
-    int byteCount = 0;
+void socketListen() {
+    struct sockaddr_in server_sockaddr_in;
+
+    const int port = 8080;
+
+    server_sockaddr_in.sin_family = AF_INET;
+    server_sockaddr_in.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_sockaddr_in.sin_port = htons(port);
+
+    int socket_file_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+
+    bind(socket_file_descriptor, (struct sockaddr *)&server_sockaddr_in, sizeof(server_sockaddr_in));
+
+    listen(socket_file_descriptor, MAX_QUEUED_REQUESTS);
+
+    int connection_file_descriptor = accept(socket_file_descriptor, NULL, 0);
+
+    char buffer[MAX_MESSAGE_SIZE] = {};
+
+    read(connection_file_descriptor, buffer, sizeof(buffer));
+    printf("%s", buffer);
+
+    char status = 0;
+    write(connection_file_descriptor, &status, sizeof(status));
+
+    close(connection_file_descriptor);
+}
+
+void getLines(FILE *fileptr) {
     char letter;
+    int byteCount = 0, lineCount = 0;
+    char byte[9];
+    char line[100];
     while (1) {
         letter = getc(fileptr);
         if (letter == EOF) {
-            byte[byteCount] = '\0';
-            printf("read: %s", byte);
-
             break;
         }
 
-        byte[byteCount] = letter;
-        byteCount++;
+        if (letter != '\n') {
+            byte[byteCount++] = letter;
+        }
 
         if (byteCount == 8) {
-            byte[byteCount] = '\0';
-            printf("read: %s\n", byte);
+            int i;
+            for (i = 0; i < 8; i++) {
+                line[lineCount++] = byte[i];
+            }
 
             byteCount = 0;
         }
+
+        if (letter == '\n') {
+            int i;
+            for (i = 0; i < byteCount; i++) {
+                line[lineCount++] = byte[i];
+            }
+
+            byteCount = 0;
+
+            line[lineCount] = '\0';
+            printf("read: %s\n", line);
+            lineCount = 0;
+        }
     }
+}
+
+int main() {
+    socketListen();
+    // FILE *fileptr = fopen("./messages.txt", "r");
+    // getLines(fileptr);
+    // fclose(fileptr);
 
     return 0;
 }
