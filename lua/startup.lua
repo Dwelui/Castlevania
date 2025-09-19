9b2019558bf0965b0f12fb6b515cea865cdc2937
Mantas Motuzas <mantas.motuzas41@gmail.com>
2025-09-19 20:24:07 +0300

    add startup

---
+++ lua/startup.lua
@@ -0,0 +1,11 @@
+shell.run("rm", "startup.lua")
+shell.run("wget", "")
+
+shell.run("rm", "chopper_v1.lua")
+shell.run("wget", "")
+
+shell.run("rm", "env.lua")
+shell.run("wget", "")
+
+shell.run("rm", "turtle_lib.lua")
+shell.run("wget", "")
