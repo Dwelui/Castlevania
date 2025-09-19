de2d43a7e1af2bac3fab37e5b0c884b181af1632
Mantas Motuzas <mantas.motuzas41@gmail.com>
2025-09-19 20:19:38 +0300

    change lua

--- lua/chopper_v1.lua
+++ lua/chopper_v1.lua
@@ -1,36 +1,12 @@
-local SERVER_DOMAIN = "http://192.168.32.8:8080"
-local URL = "/api/turtle/chopper/v1"
+local turtle_lib = require("turtle_lib")
+local env = require("env")
 
-local has_block, block_data = turtle.inspect()
-local body
-
-if has_block then
-	body = textutils.serializeJSON(block_data, true)
-else
-	body = "No block"
-end
-
-local x, y, z = gps.locate()
-local headers = {
-	["id"] = tostring(os.getComputerID()),
-	["position"] = x .. "," .. y .. "," .. z,
-}
-
-local res, err = http.post(SERVER_DOMAIN .. URL, body, headers)
+local res, err = http.post(env.SERVER_DOMAIN .. env.CHOPPER_URL, turtle_lib.vertical_blocks_data, turtle_lib.headers)
 
 if res then
 	local data = textutils.unserializeJSON(res.readAll())
 
-	for _, action in ipairs(data.actions) do
-		local fn, err = load("return " .. action)
-		if fn then
-			local ok, result1, result2 = pcall(fn)
-
-			if type(result2) == "table" then
-				result2 = textutils.serializeJSON(result2, true)
-			end
-		end
-	end
+	print(data)
 else
-	print("Failed: ", err)
+	print("ERROR: ", err)
 end
--- lua/inspect.lua
+++
@@ -1,7 +0,0 @@
-local all_block_data = {
-	["up"] = select(2, turtle.inspectUp()),
-	["front"] = select(2, turtle.inspect()),
-	["down"] = select(2, turtle.inspectDown()),
-}
-
-print(textutils.serializeJSON(all_block_data, true))
