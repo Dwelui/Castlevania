8cef1bc157e909e02451d43b9ab8da6ba97f61df
Mantas Motuzas <mantas.motuzas41@gmail.com>
2025-09-19 19:47:37 +0300

    simplify inspect

--- lua/inspect.lua
+++ lua/inspect.lua
@@ -1,11 +1,7 @@
-local up_block_exists, up_block_data = turtle.inspectUp()
-local front_block_exists, front_block_data = turtle.inspect()
-local down_block_exists, down_block_data = turtle.inspectDown()
-
 local all_block_data = {
-    ["up"] = up_block_data,
-    ["front"] = front_block_data,
-    ["down"] = down_block_data
+    ["up"] = turtle.inspectUp(),
+    ["front"] = turtle.inspect(),
+    ["down"] = turtle.inspectDown()
 }
 
-print(all_block_data)
+print(textutils.serializeJSON(all_block_data, true))
