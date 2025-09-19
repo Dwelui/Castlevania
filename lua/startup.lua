30cb1b075bf7d94eb5613253a7a5d12156143e27
Mantas Motuzas <mantas.motuzas41@gmail.com>
2025-09-19 20:25:42 +0300

    finish startup

--- lua/startup.lua
+++ lua/startup.lua
@@ -1,11 +1,11 @@
 shell.run("rm", "startup.lua")
-shell.run("wget", "")
+shell.run("wget", "https://raw.githubusercontent.com/Dwelui/Castlevania/refs/heads/main/lua/startup.lua")
 
 shell.run("rm", "chopper_v1.lua")
-shell.run("wget", "")
+shell.run("wget", "https://raw.githubusercontent.com/Dwelui/Castlevania/refs/heads/main/lua/chopper_v1.lua")
 
 shell.run("rm", "env.lua")
-shell.run("wget", "")
+shell.run("wget", "https://raw.githubusercontent.com/Dwelui/Castlevania/refs/heads/main/lua/env.lua")
 
 shell.run("rm", "turtle_lib.lua")
-shell.run("wget", "")
+shell.run("wget", "https://raw.githubusercontent.com/Dwelui/Castlevania/refs/heads/main/lua/turtle_lib.lua")
